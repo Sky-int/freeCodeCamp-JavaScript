8880f2df2c70c6411e7cf0423e1ef9a90fe0f8df
Sky-int <python.dungeon@gmail.com>
2023-04-13 18:11:15 +0300

    lesson82

--- freeCodeCamp/BasicJavaScript/lesson82/script.js
+++ freeCodeCamp/BasicJavaScript/lesson82/script.js
@@ -3,8 +3,37 @@ let count = 0;
 function cc(card) {
   // Only change code below this line
 
+var msg = '';
 
-  return "Change Me";
+switch(card) {
+  case 2:
+  case 3:
+  case 4:
+  case 5:
+  case 6:
+    count++;
+    break;
+  case 7:
+  case 8:
+  case 9:
+    count = count;
+    break;
+  case 10:
+  case "A":
+  case "J":
+  case "K":
+  case "Q":
+    count--;
+    break;
+}
+
+if(count === 5) msg ="5 Bet";
+else if(count === 0) msg = "0 Hold";
+else if(count === -5) msg = "-5 Hold";
+else if(count === -1) msg = "-1 Hold";
+else if(count === 1) msg = "1 Bet";
+
+  return msg;
   // Only change code above this line
 }
 
