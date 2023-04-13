f94e0ea214674d0ad1220ba548c9bc6fc3927702
Sky-int <python.dungeon@gmail.com>
2023-04-13 18:58:04 +0300

    lesson86

--- freeCodeCamp/BasicJavaScript/lesson86/script.js
+++ freeCodeCamp/BasicJavaScript/lesson86/script.js
@@ -1,7 +1,10 @@
-function isLess(a, b) {
-  // Only change code below this line
-    return a < b;
-  // Only change code above this line
-}
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
 
-isLess(10, 15);
+// Only change code below this line
+const playerNumber = 16;  // Change this line
+const player = testObj[playerNumber];   // Change this line
